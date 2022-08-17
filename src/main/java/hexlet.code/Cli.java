06acf384a7cfb8bc70a7cd8adb9339babe0b9d19
DTS <davidtsyganov@mail.ru>
2022-08-17 17:31:10 +0700

    Cli.greetPlayer checked

--- src/main/java/hexlet.code/Cli.java
+++ src/main/java/hexlet.code/Cli.java
@@ -6,6 +6,6 @@ public class Cli {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
         String name = scanner.nextLine();
-        System.out.println("Hello," + name + "!");
+        System.out.println("Hello, " + name + "!");
     }
 }
