b85825cdcba9c9351f5495dff1083de1d8f95e18
DTS <davidtsyganov@mail.ru>
2022-08-17 11:43:52 +0700

    Class Gcd changed. calculateGcd added changed logics in generateRound

--- src/main/java/hexlet.code/games/Gcd.java
+++ src/main/java/hexlet.code/games/Gcd.java
@@ -7,38 +7,43 @@ public class Gcd {
     private static final String TASK = "Find the greatest common divisor of given numbers.";
     private static final int UPPER_BOUND_FOR_NUMBERS = 100;
 
-    public static String[][] getResult() {
-        String[][] roundsData = new String[Engine.AMOUNT_OF_ROUNDS][2];
+    public static String[] generateRoundData() {
+        String[] roundData = new String[2];
 
-        int i = 0;
-        while (i < Engine.AMOUNT_OF_ROUNDS) {
-            int firstNumber = Utils.generateRandomNumber(UPPER_BOUND_FOR_NUMBERS);
-            int secondNumber = Utils.generateRandomNumber(UPPER_BOUND_FOR_NUMBERS);
+        int firstNumber = Utils.generateRandomNumber(UPPER_BOUND_FOR_NUMBERS);
+        int secondNumber = Utils.generateRandomNumber(UPPER_BOUND_FOR_NUMBERS);
 
-            roundsData[i][0] = firstNumber + " " + secondNumber;
+        roundData[0] = firstNumber + " " + secondNumber;
 
-            if (firstNumber == 0 || secondNumber == 0) {
-                roundsData[i][1] = "0";
-            }
+        roundData[1] = String.valueOf(calculateGCD(firstNumber, secondNumber));
 
-            int gcd = 1;
-            int edge = firstNumber <= secondNumber ? firstNumber : secondNumber;
+        return roundData;
+    }
 
-            for (int j = 1; j <= edge; j++) {
-                if (firstNumber % j == 0 && secondNumber % j == 0) {
-                    gcd = j;
-                }
-            }
+    public static int calculateGCD(int number1, int number2) {
+        int gcd = 1;
+        int edge = number1 <= number2 ? number1 : number2;
 
-            roundsData[i][1] = String.valueOf(gcd);
-
-            ++i;
+        if (number1 == 0 || number2 == 0) {
+            return 0;
         }
 
-        return roundsData;
+        for (int i = 2; i < edge; i++) {
+            if (number1 % i == 0 && number2 % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
     }
     public static void run() {
-        Engine.run(getResult(), TASK);
+        String[][] roundsData = new String[Engine.AMOUNT_OF_ROUNDS][2];
+
+        for (int i = 0; i < Engine.AMOUNT_OF_ROUNDS; i++) {
+            roundsData[i] = generateRoundData();
+        }
+
+        Engine.run(roundsData, TASK);
     }
 
 }
